2054e914a214d41118be030a675cd1a8b29c01fe
shunminli <443450050@qq.com>
2018-08-24 16:56:55 +0800

    ADD: init

---
+++ src/main/java/com/example/springdatareactiveexample/SpringDataReactiveExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.springdatareactiveexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataReactiveExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataReactiveExampleApplication.class, args);
+	}
+}
